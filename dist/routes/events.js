887220f83d258eba8f9fdb6d07e30eadfa9dcbd6
coletcarlson <colecarls13@gmail.com>
2023-01-23 00:55:14 -0600

    Added new routes for events

--- dist/routes/events.js
+++ dist/routes/events.js
@@ -8,11 +8,37 @@ router.route('/').get((req, res) => {
         .catch((err) => res.status(400).json('Error: ' + err));
 });
 //return all events that is made by a certain user
-router.route('/:userEmail').get((req, res) => {
+router.route('/createdBy/:userEmail').get((req, res) => {
     const userEmail = req.params.userEmail;
     Event.find({ userEmail: userEmail })
         .then((events) => res.json(events))
         .catch((err) => res.status(400).json('Error: ' + err));
+});
+//return all events that a user is attending
+router.route('/attending/:userEmail').get((req, res) => {
+    const userEmail = req.params.userEmail;
+    Event.find({ attendingUsers: userEmail })
+        .then((events) => res.json(events))
+        .catch((err) => res.status(400).json('Error: ' + err));
+});
+router.route('/upsert/:eventID').post((req, res) => {
+    const eventID = req.params.eventID;
+    const newEvent = req.body;
+    Event.findByIdAndUpdate(eventID, newEvent, { upsert: true })
+        .then(() => res.json('Event Updated'))
+        .catch((err) => {
+        console.log(err);
+        res.status(400).json('Error: ' + err);
+    });
+});
+router.route('/delete/:eventID').delete((req, res) => {
+    const eventID = req.params.eventID;
+    Event.findByIdAndDelete(eventID)
+        .then(() => res.json('Event Deleted'))
+        .catch((err) => {
+        console.log(err);
+        res.status(400).json('Error: ' + err);
+    });
 });
 router.route('/add').post((req, res) => {
     const eventname = req.body.eventname;
@@ -32,7 +58,7 @@ router.route('/add').post((req, res) => {
         attendingUsers
     });
     newEvent.save()
-        .then(() => res.json('Event Added!'))
+        .then(() => res.json('Event Added'))
         .catch((err) => res.status(400).json('Error: ' + err));
 });
 module.exports = router;
--- routes/events.ts
+++ routes/events.ts
@@ -9,11 +9,40 @@ router.route('/').get((req: Request, res: Response) => {
 });
 
 //return all events that is made by a certain user
-router.route('/:userEmail').get((req: Request, res: Response) => {
+router.route('/createdBy/:userEmail').get((req: Request, res: Response) => {
   const userEmail = req.params.userEmail;
   Event.find({ userEmail: userEmail })
     .then((events: typeof Event[]) => res.json(events))
     .catch((err: Error) => res.status(400).json('Error: ' + err))
+});
+
+//return all events that a user is attending
+router.route('/attending/:userEmail').get((req: Request, res: Response) => {
+  const userEmail = req.params.userEmail;
+  Event.find({ attendingUsers: userEmail })
+    .then((events: typeof Event[]) => res.json(events))
+    .catch((err: Error) => res.status(400).json('Error: ' + err))
+});
+
+router.route('/upsert/:eventID').post((req: Request, res: Response) => {
+  const eventID = req.params.eventID;
+  const newEvent = req.body;
+  Event.findByIdAndUpdate(eventID, newEvent, {upsert: true})
+    .then(() => res.json('Event Updated'))
+    .catch((err: Error) => {
+      console.log(err);
+      res.status(400).json('Error: ' + err);
+    })
+});
+
+router.route('/delete/:eventID').delete((req: Request, res: Response) => {
+  const eventID = req.params.eventID;
+  Event.findByIdAndDelete(eventID)
+    .then(() => res.json('Event Deleted'))
+    .catch((err: Error) => {
+      console.log(err);
+      res.status(400).json('Error: ' + err);
+    })
 });
 
 router.route('/add').post((req: Request, res: Response) => {
@@ -36,7 +65,7 @@ router.route('/add').post((req: Request, res: Response) => {
   });
 
   newEvent.save()
-    .then(() => res.json('Event Added!'))
+    .then(() => res.json('Event Added'))
     .catch((err: Error) => res.status(400).json('Error: ' + err))
 });
 
